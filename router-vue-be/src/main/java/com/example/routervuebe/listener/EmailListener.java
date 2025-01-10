2faa0bc127771ae85a057b85a8fa56419a91dcf4
Hue1601 <phamthihue3340@gmail.com>
2025-01-10 09:22:19 +0700

    update event listener to send email

--- router-vue-be/src/main/java/com/example/routervuebe/listener/EmailListener.java
+++ router-vue-be/src/main/java/com/example/routervuebe/listener/EmailListener.java
@@ -6,47 +6,44 @@ import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import java.util.Properties;
 
-@ControllerAdvice
+@Component
 public class EmailListener {
     @Async
     @EventListener
     public void handleSendEmail(OTPRequest request){
       try {
-          sendOtpToEmail(request.getEmail(),request.getOtp());
+          sendEmail(request.getEmail(),request.getOtp());
       }catch (Exception e){
           e.printStackTrace();
       }
     }
-    private void sendOtpToEmail(String email, String otp) {
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session = Session.getInstance(props, new Authenticator() {
+    private void sendEmail(String email, String otp) {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+        Session session = Session.getInstance(properties, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-               return new PasswordAuthentication("huethipham1601@gmail.com", "ygnx nzdh kuzo xmpw");
+                return new PasswordAuthentication("huethipham1601@gmail.com", "ygnx nzdh kuzo xmpw");
 
             }
         });
-
         try {
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress("huethipham1601@gmail.com"));
-
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-            message.setSubject("Your OTP Code");
-            message.setText("Your OTP code is: " + otp + "\nThis code is valid for 5 minutes.");
-
+            message.setSubject("YOUR OTP");
+            message.setText("Your otp is " + otp);
             Transport.send(message);
-        } catch (MessagingException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
     }
 }
